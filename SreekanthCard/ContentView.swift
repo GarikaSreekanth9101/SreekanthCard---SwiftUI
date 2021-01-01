0b3648aec62f7fdc93b6ec2ef3534a63e9a317bb
Garika Sreekanth <sreekanthgarika91@gmail.com>
2021-01-01 10:06:15 +0530

    Initial Commit

--- SreekanthCard/ContentView.swift
+++ SreekanthCard/ContentView.swift
@@ -10,12 +10,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack{
+            Color(red: 0.15, green: 0.68, blue: 0.38)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("sree")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150, height: 150)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                Text("Garika Sreekanth")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("iOS Developer")
+                    .font(.system(size: 25))
+                    .foregroundColor(.white)
+                Divider()
+                
+                InfoView(text: "+91 9010908904", imageName: "phone.fill")
+                InfoView(text: "garikasreekanth@gmail.com", imageName: "envelope.fill")
+            }
+        }
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
---
+++ SreekanthCard/InfoView.swift
@@ -0,0 +1,34 @@
+//
+//  InfoView.swift
+//  SreekanthCard
+//
+//  Created by Garika Sreekanth on 01/01/21.
+//  Copyright © 2021 Garika Sreekanth. All rights reserved.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50)
+            .overlay(HStack {
+                Image(systemName: imageName).foregroundColor(.green)
+                Text(text)
+            })
+            .padding(.all)
+    }
+}
+
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
